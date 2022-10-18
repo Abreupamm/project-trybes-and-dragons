6cf674a38122e0d84627a6114466eea0d1e97e29
Pamela <abrepamm@gmail.com>
2022-10-18 18:31:04 -0300

    classes herdeiras criada

--- src/Races/Halfling.ts
+++ src/Races/Halfling.ts
@@ -1,13 +1,13 @@
 import Race from './Race';
 
-export default class Dwarf extends Race {
+export default class Halfling extends Race {
   private _maxLifePoints: number;
 
   constructor(name: string, dexterity: number) {
     super(name, dexterity);
-    this._maxLifePoints = 80;
+    this._maxLifePoints = 60;
   }
-
+  
   get maxLifePoints(): number {
     return this._maxLifePoints;
   }
