48fb791fc87df289a8a13f97c03c3578483f4f6a
Pamela <abrepamm@gmail.com>
2022-10-19 20:03:55 -0300

    req 9

--- src/Character.ts
+++ src/Character.ts
@@ -3,6 +3,7 @@ import Race, { Elf } from './Races';
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
 import getRandomInt from './utils';
+import { SimpleFighter } from './Fighter';
 
 class Character implements Fighter {
   private _race: Race;
@@ -70,7 +71,7 @@ class Character implements Fighter {
     return this._lifePoints;
   }
 
-  attack(enemy: Fighter): void {
+  attack(enemy: Fighter | SimpleFighter): void {
     const damage = this._strength;
     enemy.receiveDamage(damage);
   }
